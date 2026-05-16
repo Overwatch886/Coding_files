1fbd93124d6da940db34708ec03103372bd7ef2c
Olawuyi Israel Mobolaji <olawuyiisrael2@gmail.com>
2026-05-16 09:58:42 +0100

    Practicing modularization

---
+++ COS203/package_exercises/main.py
@@ -0,0 +1,7 @@
+from helper import math, string
+
+width = 8
+length = 10
+area = math.area(length, width)
+result = f"The Area of a {width}-by-{length} rectangle is {area}"
+print(string.shout(result))
